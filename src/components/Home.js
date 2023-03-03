0a57c70e1722d4e61900ff0c77d7fab630c9ef1d
James Scott <jsa.codes@gmail.com>
2023-03-03 17:11:19 -0600

    Created console log in Home.js as a simple test for catArray prop

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import Header from "./Header";
 import { Typography } from "@mui/material";
 
 export default function Home({ catArray }) {
+  
+  // Testing to see if "catArray" renders correctly
+  console.log(catArray);
   return (
     <div>
       <Header />
