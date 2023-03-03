796a1bc0e7ca3eb83801f9771491ac7b93906c9f
Daniel <danchan020@gmail.com>
2023-03-03 13:41:31 -0800

    broken code

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Favorites from "./components/Favorites";
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [catObj, setCatObj] = useState([]);
+
+  useEffect(() => {
+    async function getImages() {
+      try {
+        const response = await fetch("https://api.api-ninjas.com/v1/cats", {
+          method: "GET",
+          headers: { "X-Api-Key": "OqfR/xiDH5FzbG4Dsj8WVA==Y1LlPslook0082vG" },
+          contentType: "application/json",
+        });
+        const catJSON = await response.json();
+        setCatObj(catJSON);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    getImages();
+  }, []);
+
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home catObj={catObj} />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </div>
   );
 }
-
-export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import { Typography } from "@mui/material";
 
-export default function Home() {
+export default function Home({ catObj }) {
   return (
     <div>
       <Header />
