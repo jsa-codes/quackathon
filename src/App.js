fb666713a5cf5ada1441fd0fec5e2d81a24aa0d2
Daniel <danchan020@gmail.com>
2023-03-03 16:00:00 -0800

    apicall limit 20

--- src/App.js
+++ src/App.js
@@ -10,19 +10,26 @@ export default function App() {
   useEffect(() => {
     async function getImages() {
       try {
-        const response = await fetch("https://api.api-ninjas.com/v1/cats", {
-          method: "GET",
-          headers: { "X-Api-Key": "OqfR/xiDH5FzbG4Dsj8WVA==Y1LlPslook0082vG" },
-          contentType: "application/json",
-        });
+        const response = await fetch(
+          "https://api.api-ninjas.com/v1/cats?max_weight=200",
+          {
+            method: "GET",
+            headers: {
+              "X-Api-Key": "OqfR/xiDH5FzbG4Dsj8WVA==Y1LlPslook0082vG",
+            },
+            contentType: "application/json",
+          }
+        );
         const catJSON = await response.json();
         setCatArray(catJSON);
       } catch (err) {
-        console.error(err);
+        console.log(err.message);
       }
     }
     getImages();
   }, []);
+
+  console.log(catArray);
 
   return (
     <div>
