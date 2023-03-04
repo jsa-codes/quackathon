de6b4cfa2a34b10441e84b81eca857c7309e9ed7
Auroiah Morgan <amwebdev@outlook.com>
2023-03-04 11:07:24 -0500

    Update ResultTable.jsx

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,60 +1,73 @@
-import { DataGrid } from '@mui/x-data-grid';
-import Box from '@mui/material/Box';
-import PropTypes from 'prop-types';
+import { DataGrid } from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
 
 const cols = [
-
     {
-        field: 'image_link', headerName: 'Image', width: 90, renderCell: (params) => (
-            <img src={params.value} alt={`the ${params.row.name} being displayed`} height={50} width={50} />
-        )
-    },
-    {
-        field: 'name',
-        headerName: 'Name',
-        width: 150,
-        editable: false
-    },
-
-    {
-        field: 'length',
-        headerName: 'Length',
+        field: "image_link",
+        headerName: "Image",
         width: 90,
-        editable: false
+        renderCell: (params) => (
+            <img
+                src={params.value}
+                alt={`the ${params.row.name} being displayed`}
+                height={50}
+                width={50}
+            />
+        ),
     },
     {
-        field: 'origin',
-        headerName: 'Origin',
+        field: "name",
+        headerName: "Name",
         width: 150,
-        editable: false
+        editable: false,
     },
-]
 
+    {
+        field: "length",
+        headerName: "Length",
+        width: 90,
+        editable: false,
+    },
+    {
+        field: "origin",
+        headerName: "Origin",
+        width: 150,
+        editable: false,
+    },
+];
+
+/**
+ * Takes in a array of cat data objects setting the data in appropriate cols. Allows users to store selections in localStorage.
+ * @param {array} catArray array of cat objects returned by the ninja api
+ * @returns {JSXElement} your calculated age
+ */
 export default function ResultTable({ catArray }) {
-
     return (
-        <Box sx={{ height: 400, width: '100%' }}>
-            {catArray.length > 0 ? <DataGrid
-                rows={catArray}
-                columns={cols}
-                initialState={{
-                    pagination: {
-                        paginationModel: {
-                            pageSize: 5,
-                        }
-                    }
-                }}
-                pageSizeOptions={[5]}
-                checkboxSelection
-                disableRowSelectionOnClick
-                getRowId={(row) => row?.name}
-
-            /> : <div>Loading...</div>}
+        <Box sx={{ height: 400, width: "100%" }}>
+            {catArray.length > 0 ? (
+                <DataGrid
+                    rows={catArray}
+                    columns={cols}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {
+                                pageSize: 5,
+                            },
+                        },
+                    }}
+                    pageSizeOptions={[5]}
+                    checkboxSelection
+                    disableRowSelectionOnClick
+                    getRowId={(row) => row?.name}
+                />
+            ) : (
+                <div>Loading...</div>
+            )}
         </Box>
-    )
-
+    );
 }
 
 ResultTable.propTypes = {
     catArray: PropTypes.arrayOf(PropTypes.object),
-}
+};
